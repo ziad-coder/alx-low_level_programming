e87c0cd87b4ab8a32e596e2b849d1ad89400eeda
ziad-coder <ziada5369@gmail.com>
2023-03-21 19:08:23 -0700

    functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -14,23 +14,23 @@ for (num = 0; num <= n; ++num)
 putchar(48);
 for (mult = 1; mult <= n; ++mult)
 {
-_putchar(',');
-_putchar(' ');
+putchar(',');
+putchar(' ');
 prod = num * mult;
 if (prod <= 9)
-_putchar(' ');
+putchar(' ');
 if (prod <= 99)
-_putchar(' ');
+putchar(' ');
 if (prod >= 100)
 {
-_putchar((prod / 100) + 48);
-_putchar((prod / 10) % 10 + 48);
+putchar((prod / 100) + 48);
+putchar((prod / 10) % 10 + 48);
 }
 else if (prod <= 99 && prod >= 10)
-_putchar((prod / 10) + 48);
-_putchar((prod % 10) + 48);
+putchar((prod / 10) + 48);
+putchar((prod % 10) + 48);
 }
-_putchar('\n');
+putchar('\n');
 }
 }
 }
